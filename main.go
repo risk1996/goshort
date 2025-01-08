ec570f45a4651bc48516c6e4726e4262ab0bfd84
William Darian <williamdariansutedjo@gmail.com>
2025-01-08 16:57:44 +0900

    feat: parameterize port

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/risk1996/goshort/pkg/handlers"
@@ -8,6 +11,9 @@ import (
 )
 
 func main() {
+	port := flag.Int("port", 8080, "Server port")
+	flag.Parse()
+
 	db := utils.ConnectAndMigrateDatabase("db.db")
 	r := gin.Default()
 	utils.AttachDB(r, db)
@@ -18,5 +24,5 @@ func main() {
 	r.PATCH("/:path/disable", handlers.DisableLink)
 	r.PATCH("/:path/enable", handlers.EnableLink)
 
-	r.Run(":8080")
+	r.Run(fmt.Sprintf(":%v", *port))
 }
